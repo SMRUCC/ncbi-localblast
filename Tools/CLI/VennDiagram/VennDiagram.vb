314c8d7f4dcd35f2926e4c0cc00177b9cf4b5980
xieguigang <xie.guigang@gmail.com>
2016-04-21 15:44:14 +0800

    document parser no problem

--- Tools/CLI/VennDiagram/VennDiagram.vb
+++ Tools/CLI/VennDiagram/VennDiagram.vb
@@ -1,3 +1,15 @@
-﻿Partial Module CLI
+﻿Imports LANS.SystemsBiology.Assembly.KEGG.DBGET.bGetObject
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.DocumentFormat.Csv
 
+Partial Module CLI
+
+    <ExportAPI("/SSBH2BH_LDM",
+               Usage:="/SSBH2BH_LDM /in <ssbh.csv> [/out <out.xml>]")>
+    Public Function KEGGSSOrtholog2Bh(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim out As String = args.GetValue("/out", [in].TrimFileExt & ".BestHit.Xml")
+        Dim ssbh As IEnumerable(Of SSDB.Ortholog) = [in].LoadCsv(Of SSDB.Ortholog)
+    End Function
 End Module
