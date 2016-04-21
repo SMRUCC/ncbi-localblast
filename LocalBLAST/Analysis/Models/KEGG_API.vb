33b90552dfaa4a5f67bc789dcd91305067ad3080
xieguigang <xie.guigang@gmail.com>
2016-04-21 16:39:53 +0800

    finish the tabular imports

--- LocalBLAST/Analysis/Models/KEGG_API.vb
+++ LocalBLAST/Analysis/Models/KEGG_API.vb
@@ -1,4 +1,5 @@
 ﻿Imports LANS.SystemsBiology.Assembly.KEGG.DBGET.bGetObject
+Imports Microsoft.VisualBasic.Language
 
 Namespace Analysis
 
@@ -8,7 +9,20 @@ Namespace Analysis
     Public Module KEGG_API
 
         Public Function Export(source As IEnumerable(Of SSDB.Ortholog), tag As String) As HitCollection
+            Dim hits As New HitCollection With {
+                .QueryName = tag,
+                .Hits = LinqAPI.Exec(Of SSDB.Ortholog, Hit)(source) <= Function(x) KEGG_API.__export(x)
+            }
+            Return hits
+        End Function
 
+        Private Function __export(kegg As SSDB.Ortholog) As Hit
+            Return New Hit With {
+                .HitName = kegg.hit_name,
+                .Identities = kegg.identity,
+                .Positive = kegg.identity,
+                .tag = kegg.hit_name.Split(":"c).First
+            }
         End Function
     End Module
 End Namespace
