ec3eec63e72e9b783988675b929e0c7dba6b1115
xieguigang <xie.guigang@gmail.com>
2016-04-21 17:12:33 +0800

    finish the CLI

--- Tools/CLI/VennDiagram/VennDiagram.vb
+++ Tools/CLI/VennDiagram/VennDiagram.vb
@@ -7,12 +7,21 @@ Imports Microsoft.VisualBasic.DocumentFormat.Csv
 Partial Module CLI
 
     <ExportAPI("/SSBH2BH_LDM",
-               Usage:="/SSBH2BH_LDM /in <ssbh.csv> [/out <out.xml>]")>
+               Usage:="/SSBH2BH_LDM /in <ssbh.csv> [/xml /out <out.xml>]")>
     Public Function KEGGSSOrtholog2Bh(args As CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim out As String = args.GetValue("/out", [in].TrimFileExt & ".BestHit.Xml")
-        Dim ssbh As IEnumerable(Of SSDB.Ortholog) = [in].LoadCsv(Of SSDB.Ortholog)
-        Dim Xml As HitCollection = KEGG_API.Export(ssbh, [in].BaseName)
+        Dim isXml As Boolean = args.GetBoolean("/xml")
+        Dim Xml As HitCollection
+
+        If isXml Then
+            Dim ssbh As SSDB.OrthologREST = [in].LoadXml(Of SSDB.OrthologREST)
+            Xml = KEGG_API.Export(ssbh)
+        Else
+            Dim ssbh As IEnumerable(Of SSDB.Ortholog) = [in].LoadCsv(Of SSDB.Ortholog)
+            Xml = KEGG_API.Export(ssbh, [in].BaseName)
+        End If
+
         Return Xml.SaveAsXml(out).CLICode
     End Function
 End Module
