68fd6c8617d0dd22be38e04ef5c5179c0cb7d693
xieguigang <xie.guigang@gmail.com>
2016-05-19 06:05:15 +0800

    add a score API

--- LocalBLAST/Web/MetaAPI.vb
+++ LocalBLAST/Web/MetaAPI.vb
@@ -1,6 +1,10 @@
-﻿Imports LANS.SystemsBiology.Assembly.NCBI.GenBank.TabularFormat
+﻿Imports System.Runtime.CompilerServices
+Imports LANS.SystemsBiology.Assembly.NCBI.GenBank.TabularFormat
 Imports LANS.SystemsBiology.Assembly.NCBI.GenBank.TabularFormat.ComponentModels
+Imports LANS.SystemsBiology.GenomicsContext
+Imports Microsoft.VisualBasic.DocumentFormat.Csv
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.UnixBash
 
 Namespace NCBIBlastResult
 
@@ -18,7 +22,15 @@ Namespace NCBIBlastResult
         ''' 由于在进行blast绘图的时候，程序是按照基因组来分组绘制的，而绘制的对象不需要显示详细的信息，所以在这里为True的话，会直接使用基因组tag来替换名称进而用于blast作图
         ''' </param>
         ''' <returns></returns>
-        Public Function DataParser(bbh As Analysis.BestHit, PTT As PTT, Optional visualGroup As Boolean = False) As AlignmentTable
+        Public Function DataParser(bbh As Analysis.BestHit,
+                                   PTT As PTT,
+                                   Optional visualGroup As Boolean = False,
+                                   Optional scoreMaps As Func(Of Analysis.Hit, Double) = Nothing) As AlignmentTable
+
+            If scoreMaps Is Nothing Then
+                scoreMaps = Function(x) x.Identities
+            End If
+
             Return New AlignmentTable With {
                 .Database = bbh.sp,
                 .Program = GetType(Analysis.BestHit).FullName,
@@ -31,7 +43,7 @@ Namespace NCBIBlastResult
                                                              In prot.Hits
                                                              Select New HitRecord With {
                                                                  .QueryID = prot.QueryName,
-                                                                 .Identity = hit.Identities,
+                                                                 .Identity = scoreMaps(hit),
                                                                  .AlignmentLength = ORF.Length,
                                                                  .BitScore = ORF.Length,
                                                                  .QueryEnd = ORF.Location.Ends,
@@ -42,6 +54,25 @@ Namespace NCBIBlastResult
                                                                  .DebugTag = hit.tag
                                                              }
             }
+        End Function
+
+        <Extension>
+        Public Function DensityScore(DIR As String, Optional scale As Double = 1) As Func(Of Analysis.Hit, Double)
+            Dim datas As IEnumerable(Of Density) =
+                LinqAPI.Exec(Of Density) <= From path As String
+                                            In ls - l - r - wildcards("*.csv") <= DIR
+                                            Select path.LoadCsv(Of Density)
+            Dim hash As Dictionary(Of String, Density) =
+                (From x As Density
+                 In datas
+                 Select x
+                 Group x By x.locus_tag Into Group) _
+                      .ToDictionary(Function(x) x.locus_tag,
+                                    Function(x) x.Group.First)
+            Return Function(x) _
+                If(hash.ContainsKey(x.HitName),
+                hash(x.HitName).Abundance * scale,
+                0R)
         End Function
     End Module
 End Namespace
