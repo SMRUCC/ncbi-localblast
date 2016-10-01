11a12db6dd4a76ef1d8f098f9dfc1b333d2a1036
xieguigang <xie.guigang@gmail.com>
2016-10-01 16:56:13 +0800

    rename directory

---
+++ CLI_tools/CLI/COGTools.vb
@@ -0,0 +1,95 @@
+﻿#Region "Microsoft.VisualBasic::7ddc6a324ebaf043d9ebfbdad57dbc26, ..\interops\localblast\Tools\CLI\COGTools.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Assembly.DOOR
+Imports SMRUCC.genomics.Assembly.NCBI
+Imports SMRUCC.genomics.Assembly.NCBI.COG
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.RpsBLAST
+
+Partial Module CLI
+
+    <ExportAPI("/COG.Statics",
+               Usage:="/COG.Statics /in <myva_cogs.csv> [/locus <locus.txt/csv> /locuMap <Gene> /out <out.csv>]")>
+    Public Function COGStatics(args As CommandLine) As Integer
+        Dim inFile As String = args("/in")
+        Dim locus As String = args("/locus")
+        Dim out As String
+        If Not locus.FileExists Then
+            out = args.GetValue("/out", inFile.TrimSuffix & ".COG.Stat.Csv")
+        Else
+            out = args.GetValue("/out", inFile.TrimSuffix & "." & IO.Path.GetFileNameWithoutExtension(locus) & ".COG.Stat.Csv")
+        End If
+        Dim myvaCogs = inFile.LoadCsv(Of MyvaCOG)
+
+        If locus.FileExists Then
+            Dim ext As String = IO.Path.GetExtension(locus)
+            Dim locusTag As String()
+
+            If String.Equals(ext, ".csv", StringComparison.OrdinalIgnoreCase) Then
+                Dim temp = DocumentStream.EntityObject.LoadDataSet(locus, args.GetValue("/locusMap", "Gene"))
+                locusTag = temp.ToArray(Function(x) x.Identifier)
+            Else
+                locusTag = locus.ReadAllLines
+            End If
+
+            myvaCogs = (From x As MyvaCOG In myvaCogs
+                        Where Array.IndexOf(locusTag, x.QueryName) > -1
+                        Select x).ToList
+        End If
+
+        Dim func As COG.Function = COG.Function.Default
+        Dim stst = COGFunc.GetClass(myvaCogs, func)
+        Return stst.SaveTo(out).CLICode
+    End Function
+
+    <ExportAPI("/EXPORT.COGs.from.DOOR",
+               Usage:="/EXPORT.COGs.from.DOOR /in <DOOR.opr> [/out <out.csv>]")>
+    Public Function ExportDOORCogs(args As CommandLine) As Integer
+        Dim opr As String = args("/in")
+        Dim out As String = args.GetValue("/out", opr.TrimSuffix & ".COGs.csv")
+        Dim DOOR As DOOR = DOOR_API.Load(opr)
+
+        Return (LinqAPI.MakeList(Of MyvaCOG) _
+             <= From x As GeneBrief
+                In DOOR.Genes
+                Select New MyvaCOG With {
+                    .COG = x.COG_number,
+                    .MyvaCOG = x.COG_number,
+                    .QueryName = x.Synonym,
+                    .Description = x.Product,
+                    .Category = x.COG_number
+               }) >> OpenHandle(out)
+    End Function
+
+End Module
---
+++ CLI_tools/CLI/VennDiagram/LogFile.vb
@@ -0,0 +1,129 @@
+﻿#Region "Microsoft.VisualBasic::cb7aee4bf2108fa3a3abdb96e226c2a9, ..\interops\localblast\Tools\CLI\VennDiagram\LogFile.vb"
+
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2016 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Extensions
+Imports Microsoft.VisualBasic.Terminal.STDIO
+Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.genomics.Analysis.localblast.VennDiagram.BlastAPI
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.Analysis
+Imports SMRUCC.genomics.Interops
+Imports Microsoft.VisualBasic.CommandLine
+
+Partial Module CLI
+
+    ''' <summary>
+    ''' 分析BLAST程序所输出的日志文件，目标日志文件必须是经过Grep操作得到的
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks>最后一个文件对中的File2位最后一个基因组的标号集合</remarks>
+    <ExportAPI("logs_analysis", Info:="Parsing the xml format blast log into a csv data file that use for venn diagram drawing.",
+        Usage:="logs_analysis -d <xml_logs_directory> -export <export_csv_file>",
+        Example:="logs_analysis -d ~/xml_logs -export ~/Desktop/result.csv")>
+    <ParameterInfo("-d",
+        Description:="The data directory which contains the xml format blast log file, those xml format log file were generated from the 'venn -> blast' command.",
+        Example:="~/xml_logs")>
+    <ParameterInfo("-export",
+        Description:="The save file path for the venn diagram drawing data csv file.",
+        Example:="~/Documents/8004_venn.csv")>
+    Public Function bLogAnalysis(args As CommandLine) As Integer
+        Dim CsvFile As String = args("-export")
+        Dim LogsDir As String = args("-d")
+
+        Dim ListFile = LogsPair.GetXmlFileName(LogsDir).LoadXml(Of LogsPair)()
+        Dim ListCsv = New List(Of DocumentStream.File())  '每一个文件对中的File1位主要的文件
+        For Each List In ListFile.Logs
+            Dim Query = From Pair In List
+                        Select LogAnalysis.TakeBestHits(SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.Standard.BLASTOutput.Load(Pair.File1),
+                            SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.Standard.BLASTOutput.Load(Pair.File2)) '获取BestHit
+            Call ListCsv.Add(Query.ToArray)
+        Next
+        Dim LastFile = SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.Standard.BLASTOutput.Load(ListFile.Logs.Last.Last.File2)
+        Call ListCsv.Add(New DocumentStream.File() {(From Query In LastFile.Queries.AsParallel Select Query.QueryName).ToArray})
+
+        Dim MergeResult = (From List In ListCsv Select LogAnalysis.Merge(dataset:=List)).ToList
+        Dim Csv = CLI.__mergeFile(MergeResult)  '合并文件，获取最终绘制文氏图所需要的数据文件
+
+        Return Csv.Save(Path:=CsvFile).CLICode
+    End Function
+
+    ''' <summary>
+    ''' 解析BLAST日志文件中的标记号名称
+    ''' </summary>
+    ''' <param name="args"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    <ExportAPI("grep", Info:="The gene id in the blast output log file are not well format for reading and program processing, so " &
+                                 "before you generate the venn diagram you should call this command to parse the gene id from the log file. " &
+                                 "You can also done this id parsing job using other tools.",
+        Usage:="grep -i <xml_log_file> -q <script_statements> -h <script_statements>",
+        Example:="grep -i C:\Users\WORKGROUP\Desktop\blast_xml_logs\1__8004_ecoli_prot.log.xml -q ""tokens | 4"" -h ""'tokens | 2';'tokens ' ' 0'""")>
+    <ParameterInfo("-q", False,
+        Description:="The parsing script for parsing the gene_id from the blast log file, this switch value is consist of sevral operation " &
+                     "tokens, and each token is separate by the ';' character and the token unit in each script token should seperate by " &
+                     "the ' character.\n" &
+                     "There are two basic operation in this parsing script:\n" &
+                     " tokens - Split the query or hit name string into sevral piece of string by the specific delimiter character and " &
+                     "          get the specifc location unit in the return string array.\n" &
+                     "   Usage:   tokens <delimiter> <position>\n" &
+                     "   Example: tokens | 3" &
+                     " match - match a gene id using a specific pattern regular expression.\n" &
+                     "   usage:   match <regular_expression>\n" &
+                     "   Example: match .+[-]\d{5}",
+        Example:="'tokens | 5';'match .+[-].+'")>
+    <ParameterInfo("-h",
+        Description:="The parsing script for parsing the gene_id from the blast log file, this switch value is consist of sevral operation " &
+                     "tokens, and each token is separate by the ';' character and the token unit in each script token should seperate by " &
+                     "the ' character.\n" &
+                     "There are two basic operation in this parsing script:\n" &
+                     " tokens - Split the query or hit name string into sevral piece of string by the specific delimiter character and " &
+                     "          get the specifc location unit in the return string array.\n" &
+                     "   Usage:   tokens <delimiter> <position>\n" &
+                     "   Example: tokens | 3" &
+                     " match - match a gene id using a specific pattern regular expression.\n" &
+                     "   usage:   match <regular_expression>\n" &
+                     "   Example: match .+[-]\d{5}",
+        Example:="'tokens | 5';'match .+[-].+'")>
+    Public Function Grep(args As CommandLine) As Integer
+        Dim GrepScriptQuery As TextGrepScriptEngine = TextGrepScriptEngine.Compile(args("-q"))
+        Dim GrepScriptHit As TextGrepScriptEngine = TextGrepScriptEngine.Compile(args("-h"))
+        Dim XmlFile As String = args("-i")
+
+        If String.IsNullOrEmpty(XmlFile) Then
+            Return -1
+        End If
+
+        Using File As SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.Standard.BLASTOutput =
+            SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.Standard.BLASTOutput.Load(XmlFile) 'Depose 操作的时候会自动保存
+            Call File.Grep(Query:=AddressOf GrepScriptQuery.Grep, Hits:=AddressOf GrepScriptHit.Grep)
+        End Using
+
+        Return 0
+    End Function
+End Module
