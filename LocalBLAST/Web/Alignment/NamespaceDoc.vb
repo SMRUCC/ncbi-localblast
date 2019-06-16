2eec43146adb73820b6f28131883e698ce08acb5
xieguigang <xie.guigang@gcmodeller.org>
2019-06-16 19:12:47 +0800

    prepare for improvements code of NCBI web blast output processor

---
+++ LocalBLAST/Web/Alignment/NamespaceDoc.vb
@@ -0,0 +1,8 @@
+﻿Namespace NCBIBlastResult
+
+    ''' <summary>
+    ''' File parser for alignment text output from NCBI web application.
+    ''' </summary>
+    Module NamespaceDoc
+    End Module
+End Namespace
